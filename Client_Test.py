18e3d078984d0276cdfd980f9a2e6f3642ced6fb
TobiasKleinhansl <tobias.kleinhansl@student.reutlingen-university.de>
2020-12-10 20:34:28 +0100

    TCP Versuch - bei mir klappts nicht wegen Errno 61

--- Client_Test.py
+++ Client_Test.py
@@ -1,6 +1,6 @@
 import socket
-broadcast_address = "192.168.2.85"
-udp_serverport = 1000
+broadcast_address = "192.168.0.220"
+udp_serverport = 1234
 tcp_serverport = 1235
 buffer = 1024
 
@@ -8,31 +8,63 @@ buffer = 1024
 #UDP connection
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-client_name = input("Ihr Name?")
+nickname = input("Ihr Name?")
 
-while True:
-    udp_socket.sendto(str.encode(client_name), (broadcast_address, udp_serverport))
-    print("Requesting blackboard entrance.")
+
+udp_socket.sendto(str.encode(nickname), (broadcast_address, udp_serverport))
+print("Requesting blackboard entrance.")
 
 host_address = udp_socket.recv(buffer)
 print("Hostaddress is {}".format(host_address))
 
-#udp_socket.close()
+udp_socket.close()
 
 
 #TCP connection
 
-tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_socket.connect((host_address, tcp_serverport))
+import socket
+import threading
+import sys
+#source https://www.neuralnine.com/tcp-chat-in-python/
+#source https://pymotw.com/2/socket/tcp.html
+# Nutzernamen auswaehlen
+#nickname = input("Wie lautet ihr Benutzername? ")
 
-#est = tcp_socket.recv(buffer)
-#print(est)
+# Verbindung zum Server
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#server_address = ("172.29.180.18", 10000)
+#print('Verbindung mit dem Blackboard auf %s mit Port %s herstellen' % host_address)
+print(host_address)
 
-#if est == "Connection established." :
-    #receive last 10 messages
-    
-while True:
-    new_entry = input("Eintrag hinzufuegen:")
-    tcp_socket.send(str.encode(new_entry))
-    
+sock.connect((host_address, tcp_serverport))
+
+# Dem Server zuhoeren und den Benutzernamen senden
+def receive():
+    while True:
+        try:
+            # Receive Message From Server
+            # If 'NICK' Send Nickname
+            message = sock.recv(1024).decode('ascii')
+            if message == 'NICK':
+                sock.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            # Close Connection When Error
+            print("Ein Fehler ist aufgetreten!")
+            sock.close()
+            break
+        
+# Nachrichten zum Server senden
+def write():
+    while True:
+        message = '{}: {}'.format(nickname, input(''))
+        sock.send(message.encode('ascii'))
+        
+# 2 Threads fuer Zuhoeren und Nachrichten schreiben starten
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
     
--- Server_Test1.py
+++ Server_Test1.py
@@ -1,56 +1,93 @@
-
 import socket
 buffer = 1024
 clientdict = {}
-bind_address = "192.168.2.85"
+
 
 #UDP connection
 
 udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_socket.bind((bind_address, 1023))
+udp_socket.bind(("192.168.0.220", 1234))
 
-
-udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-#udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+#tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
     
     # Enable broadcasting mode
-#udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+#tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
-print("Server up and running...")
+print("server up and running...")
 
 #while True:
-print("Waiting for client request... on {}".format(bind_address))
-
-  
-while True:
-    print("empfangen laeuft")
-    data, addr = udp_socket.recv(1024)
-    print("Message: ", data)
-
-#client_name, client_address = udp_socket.recvfrom(buffer)
-#clientdict[client_address] = client_name
-
-#print("client request received from client {} on IP {}".format(client_name, client_address))
-#print("Establishing connection")
+print("Waiting for client request...")
+client_name, client_address = udp_socket.recvfrom(buffer)
+clientdict[client_address] = client_name
+print("client request received from client {} on IP {}".format(client_name, client_address))
+print("Establishing connection")
     
-#udp_socket.sendto(str.encode(bind_address), client_address) #hier muss noch der port mitgeschickt werden!
+udp_socket.sendto(str.encode("192.168.0.220"), client_address)
 
-#udp_socket.close()  #Ansonsten errno 48: address already in use
+udp_socket.close()  #Ansonsten errno 48: address already in use
   
 
 #TCP connection  
-    
-#tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-#tcp_socket.bind(("192.168.0.221", 1235))
-#tcp_socket.listen()
 
-#tcp_socket.accept()
-#tcp_socket.send(str.encode("Connection established.")
-print("Connection established.")
+import threading
+import socket
+import sys
+from test.test_decimal import file
 
-#sender, entry = tcp_socket.recv(buffer)
-#tcp_socket.sendto("{} wrote: {}".format(clientdict[sender], entry), clientdict.keys()) #sollte das funktionieren dann nur für einen Client im dictionary
+# Create a TCP/IP socket
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-  
+# Bind the socket to the address given on the command line
+server_name = "192.168.0.220"
+#server_name = socket.gethostbyname(socket.gethostname())
+server_address = (server_name, 10000)
+print('Server gestartet auf %s mit Port %s' % server_address)
+server.bind(server_address)
+server.listen(1)
+
+clients = []
+nicknames = []
+
+def broadcast(message): #um Nachrichten  zu den Clients zu senden
+    for client in clients:
+        client.send(message)
+
+def handle(client): #Fuer jeden Client auf dem Server wird ein eigener handle aufgerufen in jedem einzelnen Thread
+    while True:
+        try:
+            message = client.recv(1024) #Nachricht empfangen
+            broadcast(message) #Wenn eine Nachricht angekommen ist, wird die Nachricht an die anderen Clients gebroadcastet
+
+        
+        except: #Sofern der Client keine Nachricht empfaengt
+            index = clients.index(client)
+            clients.remove(client) #Client wird von der Clientlist entfernt
+            client.close()
+            nickname = nicknames[index]
+            broadcast(f'{nickname} hat das Blackboard verlassen'.encode('ascii'))
+            nicknames.remove(nickname)
+            break
+
+def receive():
+    while True:
+        # Die Verbindung akzeptieren
+        client,server_address = server.accept()
+        print("Verbunden mit {}".format(str(server_address)))
+
+        # Anforderung des Benutzernamens und Speicherung dessen
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+
+        # Benutzername mitteilen und broadcasten
+        print("Der Benutzername ist {}".format(nickname))
+        broadcast("{} ist dem Blackboard beigetreten!".format(nickname).encode('ascii'))
+        client.send('Mit dem Server verbunden!'.encode('ascii'))
+
+
+        # Start Handling Thread For Client
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+                
+receive()
