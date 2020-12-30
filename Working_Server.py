c800ae18a2392cae653f957ecdcc851e636c597b
lucasfri <36990604+lucasfri@users.noreply.github.com>
2020-12-30 17:22:56 +0100

    push

--- Working_Server.py
+++ Working_Server.py
@@ -5,55 +5,74 @@ from sys import platform as _platform
 from turtledemo.minimal_hanoi import Disc
 from _socket import SHUT_RDWR
 
+
 server_ip = "192.168.178.41"
 broadcast_ip = "192.168.178.255"
-discovery_port = 1999
+discovery_port = 1236
 udp_port = 1234
 tcp_port = 1235
 buffer = 1024
-
-server_list = []
+leader = 1
 
 def server_discovery():
 
-  #  host_udp_socket.bind((server_ip, udp_port))
-    #check mithilfe einer if-anweisung ob das Betriebssystem Win oder Unixbasiert ist
-    if _platform == "linux" or _platform == "linux2" or _platform == "darwin": 
-        host_udp_socket.bind((broadcast_ip, discovery_port))
-        # linux 
-    elif _platform == "win32" or _platform == "win64":
-         host_udp_socket.bind((server_ip, discovery_port))
-    
     print("Ich habe erkannt, dass du das folgende Betriebssystem hast: ", _platform)
 
     #meine serverinformation an andere server schicken
     data = "%s:%s" % ("SA", server_ip)
-    host_udp_socket.sendto(str.encode(data), (broadcast_ip, 1236))
+    host_udp_socket.sendto(str.encode(data), (broadcast_ip, discovery_port))
     print("Following was broadcasted: ", data)
-    
-   # wenn SA... Nachricht empfangen habe, schicke ich eine SB Nachricht an den Sender mit meiner IP POrt zurueck
    
-    print("Waiting for server request...") 
-       
-    #sa_message = host_udp_socket.recv(buffer)
-    #if sa_message str.startswith(SA)
-    #print("client request received from client on IP {}".format(client_address))
+    if leader == 1:
+        Thread(target=listen_for_servers, args=()).start()
+        
+    Thread(target=client_discovery, args=()).start()
+    host_udp_socket.close()
 
+def listen_for_servers():    
+   # wenn SA... Nachricht empfangen habe, schicke ich eine SB Nachricht an den Sender mit meiner IP POrt zurueck
+    #listen_for_servers_socket.bind((server_ip, 1236))
+    ##check mithilfe einer if-anweisung ob das Betriebssystem Win oder Unixbasiert ist
+    
+    listen_for_servers_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    listen_for_servers_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    listen_for_servers_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    
+    if _platform == "linux" or _platform == "linux2" or _platform == "darwin": 
+        listen_for_servers_socket.bind((broadcast_ip, discovery_port))
+        # linux 
+    elif _platform == "win32" or _platform == "win64":
+        listen_for_servers_socket.bind((server_ip, discovery_port))
+
+    #checken ob ein server acknowledgement vorliegt
+    sa_message = listen_for_servers_socket.recv(buffer).decode("UTF-8")
+    listen_for_servers_socket.close()
+    
+    
+    if sa_message[:2] == "SA":
+        print("server request received from server on IP {}".format(sa_message))
+        located_server_ip = sa_message[3:]
+        server_list.append(located_server_ip)
+        
+        #als leader die serverliste an die anderen server senden
+        
+#5 sekunden darauf warten, dass der leader mir die client list sendet
     #infos von anderen servern empfangen
-    timeout = time.time() + 1
-    data = bytearray()
-    new_server = memoryview(data)
+    #timeout = time.time() + 1
+    #data = bytearray()
+    #new_server = memoryview(data)
 
-    while time.time() < timeout:
+
+    #while time.time() < timeout:
         #try:
-        print("test0")
-        host_udp_socket.recv_into(new_server)
-        server_list.append(new_server)
-        print("receiving")
+    #    print("listening for other servers responses...") 
+    #    host_udp_socket.recv_into(new_server)
+    #    server_list.append(new_server)
+    #    print("receiving")
        # except:
        #     print("Listening for Servers")
        
-    Thread(target=client_discovery, args=()).start()
+    Thread(target=listen_for_servers(), args=()).start()
 
 
 #Create UDP socket, listen for broadcast, transmit own address
@@ -75,9 +94,7 @@ def client_discovery():
        
     request, client_address = udp_socket.recvfrom(buffer)
     print("client request received from client on IP {}".format(client_address))
-    
-    udp_socket.close()
-    
+        
     udp_socket2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
@@ -89,6 +106,21 @@ def client_discovery():
     Thread(target=connect, args=()).start()
 
 def connect():
+    
+        #TCP connection  
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    #server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    #server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    
+    
+    #server_ip = socket.gethostbyname(socket.gethostname())
+    server_address = (server_ip, tcp_port)
+    print('Server gestartet auf IP %s und Port %s' % server_address)
+    server.bind(server_address)
+    server.listen()
+    
+    
     # Die Verbindung akzeptieren
     client,client_address = server.accept()
     print("Verbunden mit client {}".format(str(client_address)))
@@ -125,23 +157,13 @@ def messaging(client): #Fuer jeden Client auf dem Server wird ein eigener handle
         nickname = nicknames[index]
         broadcast(f'{nickname} hat das Blackboard verlassen'.encode('ascii'))
         nicknames.remove(nickname)
-        
+    Thread(target=messaging(client), args=(client)).start()
+    
 
 if __name__ == "__main__":
     
-    #TCP connection  
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-    #server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
-    #server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    
-    
-    #server_ip = socket.gethostbyname(socket.gethostname())
-    server_address = (server_ip, tcp_port)
-    print('Server gestartet auf IP %s und Port %s' % server_address)
-    server.bind(server_address)
-    server.listen()
-    
+
+    server_list = []
     clients = []
     nicknames = []
     messages = []
@@ -152,7 +174,7 @@ if __name__ == "__main__":
     #tcp_thread.start()
     host_udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     host_udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    
+
     
     ACCEPT_THREAD = Thread(target=server_discovery)
 
