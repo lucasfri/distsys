1964c884275f8400d41d2d9cc3727815df28c572
lucasfri <36990604+lucasfri@users.noreply.github.com>
2021-01-04 12:09:01 +0100

    ss

--- Client_Test.py
+++ Client_Test.py
@@ -1,46 +1,15 @@
 import socket
-import socket
-import threading
+from threading import Thread
 import pickle
 
-broadcast_ip = "192.168.0.255"
+broadcast_ip = "192.168.2.255"
 udp_serverport = 1234
 tcp_serverport = 1235
 buffer = 1024
 
-#UDP connection
-
-udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-
-nickname = input("Bitte beliebige Eingabe um Verbindungsaufbau zu starten.")
-
-udp_socket.sendto(str.encode(nickname), (broadcast_ip, udp_serverport))
-print("Requesting blackboard entrance.")
-
-host_address = udp_socket.recv(buffer)
-print("Hostaddress is {}".format(host_address))
-
-udp_socket.close()
-
-
-#TCP connection
-
-#source https://www.neuralnine.com/tcp-chat-in-python/
-#source https://pymotw.com/2/socket/tcp.html
-# Nutzernamen auswaehlen.
-nickname = input("Wie lautet ihr Benutzername? ")
-
-# Verbindung zum Server
-tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-tcp_socket.connect((host_address, tcp_serverport))
-print("TCP connection with server on IP {} established.".format(host_address))
-
-
 # Dem Server zuhoeren und den Benutzernamen senden
 def receive():
+    #Thread(target=send(nickname), args=(nickname, )).start()
     while True:
         try:
             # Receive Message From Server
@@ -59,13 +28,48 @@ def receive():
 # Nachrichten zum Server senden
 def send():
     while True:
+        print("send")
         message = '{}: {}'.format(nickname, input(''))
         tcp_socket.send(message.encode('ascii'))
+    
 
-# 2 Threads fuer Zuhoeren und Nachrichten schreiben starten
-receive_thread = threading.Thread(target=receive)
-receive_thread.start()
+if __name__ == "__main__":
 
-write_thread = threading.Thread(target=send)
-write_thread.start()
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+#UDP connection
+    
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    udp_socket.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    
+    
+    nickname = input("Bitte beliebige Eingabe um Verbindungsaufbau zu starten.")
+    
+    udp_socket.sendto(str.encode(nickname), (broadcast_ip, udp_serverport))
+    print("Requesting blackboard entrance.")
+    
+    host_address = udp_socket.recv(buffer)
+    print("Hostaddress is {}".format(host_address))
+    
+    udp_socket.close()
+
+    #TCP connection
+    
+    #source https://www.neuralnine.com/tcp-chat-in-python/
+    #source https://pymotw.com/2/socket/tcp.html
+    # Nutzernamen auswaehlen.
+    nickname = input("Wie lautet ihr Benutzername? ")
+    
+    # Verbindung zum Server
+    
+    tcp_socket.connect((host_address, tcp_serverport))
+    print("TCP connection with server on IP {} established.".format(host_address))
+
+    # 2 Threads fuer Zuhoeren und Nachrichten schreiben starten
+    receive_thread = Thread(target=receive)
+    receive_thread.start()
+
+    write_thread = Thread(target=send)
+    write_thread.start()
 
