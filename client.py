587e1dc099d850f6fe9a2e437eccb1e5664b4845
TobiasKleinhansl <tobias.kleinhansl@student.reutlingen-university.de>
2021-01-17 22:00:15 +0100

    Small adjustment to tcp socket

--- client.py
+++ client.py
@@ -50,7 +50,7 @@ def udp():
 #Connect with lead server which was identified via udp()
 def tcp():
     
-    global tcp_sockets
+    global tcp_socket
     global nickname
     
     tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -60,7 +60,7 @@ def tcp():
     tcp_socket.connect((host_address, tcp_serverport))
     print("TCP connection with server on IP {} established.".format(host_address))
     
-    tcp_sockets.append(tcp_socket)
+    #tcp_sockets.append(tcp_socket)
     
     Thread(target=heartbeat, args=()).start()
     time.sleep(2)
@@ -73,41 +73,40 @@ def tcp():
 #receive messages from lead server
 def receive():
 
-    global tcp_sockets
+    global tcp_socket
     global nickname
     
     while True:
         if stop_threads ==  True: break
         else:
-            for element in tcp_sockets:
-                message = element.recv(buffer).decode('ascii')
-                if message == 'NICK':
-                    element.send(nickname.encode('ascii'))
-                else:
-                    if len(message) != 0:
-                        print(message)
-
+            #for element in tcp_sockets:
+            message = tcp_socket.recv(buffer).decode('ascii')
+            if message == 'NICK':
+                tcp_socket.send(nickname.encode('ascii'))
+            else:
+                if len(message) != 0:
+                    print(message)
             
             
 
 # Send user input to lead server
 def send():
     global stop_threads
-    global tcp_sockets
+    global tcp_socket
     
     while True:
         if stop_threads ==  True: break
         else:
             try:
-                for element in tcp_sockets:
-                    print("Enter message:")
-                    message = '{}: {}'.format(nickname, input(''))
-                    element.send(message.encode('ascii'))
+                #for element in tcp_sockets:
+                print("Enter message:")
+                message = '{}: {}'.format(nickname, input(''))
+                tcp_socket.send(message.encode('ascii'))
             except:
                 print("Connection to Leader lost. Please try again.")
                 stop_threads = True
                 time.sleep(3)
-                tcp_sockets = []
+                #tcp_socket = ""
                 udp()
                 break
 
@@ -118,7 +117,7 @@ def heartbeat():
 
     global host_address
     global client_ip
-    global tcp_sockets
+    global tcp_socket
     
     heartbeat_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     heartbeat_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -140,9 +139,8 @@ def heartbeat():
         except:
             print("Connection to Leader lost")
             host_address = ""
-            for element in tcp_sockets:
-                element.close()
-            tcp_sockets = []
+            tcp_socket.close()
+            #tcp_sockets = ""
             stop_threads = True
             time.sleep(3)
             udp()
@@ -165,10 +163,13 @@ if __name__ == "__main__":
     buffer = 1024
     host_address = ""
     stop_threads = False
-    tcp_sockets = []
     nickname = ""
 
     nickname = input("Enter username:")
     
     udp()
 
+
+#heartbeat time.sleep
+#multicast -->
+# tcp_sockets zu Variable
